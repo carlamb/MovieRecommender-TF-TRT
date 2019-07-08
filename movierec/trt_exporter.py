b2f7ca946e286fcb09314bbd6dc7be877be9e893
carla <6702410+carlamb@users.noreply.github.com>
2019-07-08 09:01:17 -0400

    Make sure exporting to TensorRT works for Tensorflow versions < 1.14
    - Add local copy of Tensorflow 1.14 graph_util
    - Small fixes in exporting method

--- movierec/trt_exporter.py
+++ movierec/trt_exporter.py
@@ -1,10 +1,20 @@
 """ Export models. """
 
+import logging
 import tensorflow as tf
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.models import load_model
 import os
-from tensorflow.python.framework.graph_io import write_graph
+from tensorflow.python.framework import graph_io
+
+# For versions older than 1.14, use graph_utils from 1.14 (locally downloaded)
+tf_version = tf.__version__.split('.')
+if tf_version[0] == '1' and int(tf_version[1]) < 14:
+    logging.warn("Importing graph_util from local copy because tensorflow version {} is lower than 1.14"
+                 .format(tf.__version__))
+    from util import tf_graph_util
+else:
+    import tensorflow.compat.v1.graph_util as tf_graph_util
 
 
 def export_keras_model_to_trt(model_file, output_dir):
@@ -21,26 +31,31 @@ def export_keras_model_to_trt(model_file, output_dir):
         Directory to save the exported model.
 
     """
-
-    model = load_model(model_file)
-    session = K.get_session()
-
     # set learning phase to 'test'
     K.set_learning_phase(0)
 
-    print(model.outputs)
-    frozen_graph = tf.compat.v1.graph_util.convert_variables_to_constants(
-            sess=session,
-            input_graph_def=session.graph.as_graph_def(),
-            output_node_names=[out.op.name for out in model.outputs])
-    tf.compat.v1.graph_util.remove_training_nodes(frozen_graph)
+    model = load_model(model_file)
 
-    # write to dir following TensortRT naming requirements
-    write_graph(
-            graph_or_graph_def=frozen_graph,
-            logdir=os.path.join(output_dir, '1'),
-            name='model.graphdef',
-            as_text=False)
+    with K.get_session() as session:
+        graph = session.graph
+        with graph.as_default():
+            graph_def = graph.as_graph_def()
+
+            # freeze graph and save to file
+            frozen_graph = tf_graph_util.remove_training_nodes(graph_def)
+            frozen_graph = tf_graph_util.convert_variables_to_constants(
+                sess=session,
+                input_graph_def=frozen_graph,
+                output_node_names=[out.op.name for out in model.outputs])
+            # dir structure and name as expected by TensorRT
+            graph_io.write_graph(
+                graph_or_graph_def=frozen_graph,
+                logdir=os.path.join(output_dir, '1'),
+                name='model.graphdef',
+                as_text=False)
+
+
+
 
 
 if __name__ == '__main__':
